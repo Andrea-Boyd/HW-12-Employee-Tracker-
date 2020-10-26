2d3b4615ecf5637ad01efb0f71a84e094f2a670d
Andrea Boyd <andreanboyd@yahoo.com>
2020-10-26 17:17:57 -0500

    Added displayRoles and displayEmployees functions; added preload function to handle async issues; added updateRoles function as well as updateRoleTable function

--- index.js
+++ index.js
@@ -4,14 +4,8 @@ const cTable = require("console.table");
 
 var connection = mysql.createConnection({
     host: "localhost",
-
-    // Your port; if not 3306
     port: 3306,
-
-    // Your username
     user: "root",
-
-    // Your password
     password: "056abg$%",
     database: "employee_db"
 });
@@ -92,6 +86,82 @@ function displayDepartments() {
         console.table(department);
         startApp();
 })};
+
+function displayRoles() {
+    let query = "SELECT * FROM role";
+    connection.query(query, async function(err, res) {
+        if (err) throw err;
+        let role = await res;
+        console.table(role);
+        startApp();
+})};
+
+function displayEmployees() {
+    let query = "SELECT * FROM employee";
+    connection.query(query, async function(err, res) {
+        if (err) throw err;
+        let employee = await res;
+        console.table(employee);
+        startApp();
+})};
+
+var roleArr= [];
+var nameArr= [];
+function preLoad() {
+    roleArr = [];
+    nameArr = [];
+    let query = "SELECT * FROM employee";
+    connection.query(query, async function (err, res) {
+        if (err) throw err;
+        let employee = await res;
+        for (i=0; i<employee.length; i++) {
+            nameArr.push(employee[i].first_name + " " + employee[i].last_name)
+        }
+    })
+
+    let queryTwo = "SELECT * FROM role";
+    connection.query(queryTwo, async function(err, res) {
+        if(err) throw err;
+        let role = await res;
+        for (j=0; j<role.length; j++){
+            roleArr.push(role[j].title)
+        }
+    })
+};
+
+preLoad();
+
+function updateRoles() {
+    prompt({
+        name: "update",
+        type: "list",
+        message: "Which employee would you like to update?",
+        choices: nameArr
+    }).then(function (resName) {
+        prompt ({
+        name: "role",
+        type: "list",
+        message: "What would you like to view?",
+        choices: roleArr
+    }).then(function (resRole) {
+        var employeeID = nameArr.indexOf(resName.update) +1;
+        var roleID = roleArr.indexOf(resRole.role) +1;
+        updateRoleTable(employeeID, roleID)
+    })
+})
+};
+
+function updateRoleTable(name, role) {
+    let query = "UPDATE employee SET role_id = ? WHERE employee.id = ?"
+    connection.query(query, [role, name], (err, res) => {
+        if(err) throw err;
+        console.log("\nUpdated, bro");
+        // preLoad();
+        startApp();
+    })
+};
+
+
 
 
 
