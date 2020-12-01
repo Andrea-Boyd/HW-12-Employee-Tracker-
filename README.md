a9cf2eab8b0f9ec4e28377099e2ca14b5174181b
Andrea Boyd <andreanboyd@yahoo.com>
2020-12-01 14:50:53 -0600

    updated readme, removed password from index, finished functionality of adding dept/role/employee

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # HW-12-Employee-Tracker-
+
+This is a command-line application that allows the user to:
+
+  * Add departments, roles, employees
+
+  * View departments, roles, employees
+
+  * Update employee roles
+
+  ```
+As a business owner
+I want to be able to view and manage the departments, roles, and employees in my company
+So that I can organize and plan my business
+```
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "056abg$%",
+    password: " ",
     database: "employee_db"
 });
 
@@ -70,7 +70,7 @@ function viewChoices() {
             case "Employees":
                 displayEmployees();
                 break;
-            
+
             case "Return to Main Menu":
                 startApp();
                 break;
@@ -80,33 +80,36 @@ function viewChoices() {
 
 function displayDepartments() {
     let query = "SELECT * FROM department";
-    connection.query(query, async function(err, res) {
+    connection.query(query, async function (err, res) {
         if (err) throw err;
         let department = await res;
         console.table(department);
         startApp();
-})};
+    })
+};
 
 function displayRoles() {
     let query = "SELECT * FROM role";
-    connection.query(query, async function(err, res) {
+    connection.query(query, async function (err, res) {
         if (err) throw err;
         let role = await res;
         console.table(role);
         startApp();
-})};
+    })
+};
 
 function displayEmployees() {
     let query = "SELECT * FROM employee";
-    connection.query(query, async function(err, res) {
+    connection.query(query, async function (err, res) {
         if (err) throw err;
         let employee = await res;
         console.table(employee);
         startApp();
-})};
+    })
+};
 
-var roleArr= [];
-var nameArr= [];
+var roleArr = [];
+var nameArr = [];
 function preLoad() {
     roleArr = [];
     nameArr = [];
@@ -114,16 +117,16 @@ function preLoad() {
     connection.query(query, async function (err, res) {
         if (err) throw err;
         let employee = await res;
-        for (i=0; i<employee.length; i++) {
+        for (i = 0; i < employee.length; i++) {
             nameArr.push(employee[i].first_name + " " + employee[i].last_name)
         }
     })
 
     let queryTwo = "SELECT * FROM role";
-    connection.query(queryTwo, async function(err, res) {
-        if(err) throw err;
+    connection.query(queryTwo, async function (err, res) {
+        if (err) throw err;
         let role = await res;
-        for (j=0; j<role.length; j++){
+        for (j = 0; j < role.length; j++) {
             roleArr.push(role[j].title)
         }
     })
@@ -138,31 +141,176 @@ function updateRoles() {
         message: "Which employee would you like to update?",
         choices: nameArr
     }).then(function (resName) {
-        prompt ({
-        name: "role",
-        type: "list",
-        message: "What would you like to view?",
-        choices: roleArr
-    }).then(function (resRole) {
-        var employeeID = nameArr.indexOf(resName.update) +1;
-        var roleID = roleArr.indexOf(resRole.role) +1;
-        updateRoleTable(employeeID, roleID)
+        prompt({
+            name: "role",
+            type: "list",
+            message: "What role would you like to assign this employee?",
+            choices: roleArr
+        }).then(function (resRole) {
+            var employeeID = nameArr.indexOf(resName.update) + 1;
+            var roleID = roleArr.indexOf(resRole.role) + 1;
+            updateRoleTable(employeeID, roleID)
+        })
     })
-})
 };
 
 function updateRoleTable(name, role) {
     let query = "UPDATE employee SET role_id = ? WHERE employee.id = ?"
     connection.query(query, [role, name], (err, res) => {
-        if(err) throw err;
+        if (err) throw err;
         console.log("\nUpdated, bro");
         // preLoad();
         startApp();
     })
 };
 
+function addChoices() {
+    prompt({
+        name: "add",
+        type: "list",
+        message: "What would you like to add?",
+        choices: [
+            "New Department",
+            "New Role",
+            "New Employee",
+            "Return to Main Menu"
+        ]
+    }).then(function (res) {
+        switch (res.add) {
+            case "New Department":
+                addNewDepartment();
+                break;
+
+            case "New Role":
+                addNewRole();
+                break;
+
+            case "New Employee":
+                addNewEmployee();
+                break;
+
+            case "Return to Main Menu":
+                startApp();
+                break;
+        }
+    })
+};
 
 
 
+function addNewDepartment() { 
+
+        prompt({
+            name: "name",
+            type: "input",
+            message: "Enter the Name of the New Department: "
+        
+        }).then(function (res) {
+            connection.query(
+                "INSERT INTO department SET ?",
+                {
+                    name: res.name,
+                },
+                function (err) {
+                    if (err) throw err;
+                    startApp();
+                }
+            );
+        });
+    };
+
+
+
+function addNewRole() {
+    let query = "SELECT * FROM department";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        var depts = res;
+
+        prompt([{
+            name: "title",
+            type: "input",
+            message: "Enter the Title of the new Role: "
+        },
+        {
+            name: "salary",
+            type: "input",
+            message: "Enter the Salary for this new Role: "
+        },
+        {
+            name: "id",
+            type: "list",
+            message: "Select the Department to which the new Role will Belong: ",
+            choices: depts.map(({id, name}) => ({name: name, value: id}))
+        }]).then(function (res) {
+            connection.query(
+                "INSERT INTO role SET ?",
+                {
+                    title: res.title,
+                    salary: res.salary,
+                    department_id: res.id
+                },
+                function (err) {
+                    if (err) throw err;
+                    startApp();
+                }
+            );
+        });
+    });
+};
+
+
+function addNewEmployee() {
+    let query = "SELECT * FROM role";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        var roles = res;
+
+    let query = "SELECT * FROM employee";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        var emps = res;
+
+        prompt([{
+            name: "first",
+            type: "input",
+            message: "Enter the Employee's First Name: "
+        },
+        {
+            name: "last",
+            type: "input",
+            message: "Enter the Employee's Last Name: "
+        },
+        {
+            name: "role",
+            type: "list",
+            message: "Select the Role to which the new Employee will Belong: ",
+            choices: roles.map(({id, title}) => ({name: title, value: id}))
+        },
+        {
+            name: "manager",
+            type: "list",
+            message: "Select the Manager to which the new Employee will Report: ",
+            choices: emps.map(({id, first_name, last_name}) => ({name: [first_name + " " + last_name], value: id}))
+        }
+    
+    ]).then(function (res) {
+            connection.query(
+                "INSERT INTO employee SET ?",
+                {
+                    first_name: res.first,
+                    last_name: res.last,
+                    role_id: res.role,
+                    manager_id: res.manager
+                },
+                function (err) {
+                    if (err) throw err;
+                    startApp();
+                }
+            );
+        });
+    });
+});
+};
 
 
